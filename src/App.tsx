2d8264745ad07bd66abca35a0aafc4c727926567
ovoronkov-dev <ovoronkov.dev@gmail.com>
2023-03-14 00:43:45 +0200

    feat: add material provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import { AuthLayout } from "~layouts/AuthLayout";
+import { defaultTheme } from "~styles/theme";
 import { Dashboard } from "~views/Dashboard";
 import { Login } from "~views/Login";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AuthLayout />}>
-        <Route index element={<Dashboard />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
